26e2db94428bfdadc1670297a77d1b1cc93f90cd
ajgist <59585641+ajgist@users.noreply.github.com>
2022-05-12 19:25:55 -0500

    winners are more accurate, noms is WIP

--- gg_api.py
+++ gg_api.py
@@ -5,27 +5,50 @@ import pandas as pd
 import import_ipynb
 import preprocess_csv
 from preprocess_csv import preprocess
+import nltk
+import nltk.data
+from nltk.corpus import stopwords as sw
+
+
 
 OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
 
+aw = []
+#stopwords = ["to", "and", "I", "that", "this", "for", "the", "an", "at"]
+stopwords = list(sw.words("english"))[:100]
 
 #helper funcs ----------------
-def sortCandidates(candidates):
-    c = nltk.FreqDist([item for sublist in candidates for item in sublist])
-    return c.max(1)
-    # top_3 = = c.keys()[:3] - for ranked list
+def sortCandidates(candidates, number, award):
+    arrCombo = []
+    for sublist in candidates:
+        arrCombo = arrCombo + sublist
+
+    c = nltk.FreqDist(arrCombo)
+    topList = c.most_common(20)
+
+    #print("RANKING === ", topList)
+
+    topList2 = [word[0] for word in topList]
+    print("test tops", topList2)
+    print(' -------------- ')
+    #tags = nltk.pos_tag(topList2) only look for names for categories with actor/actress/director/etc
+    if number == 1: return c.max()
+    else: return topList2[:number]
+
+    # top_3 = c.keys()[:3] - for ranked list
 
             
 def addRight(tweet, index):
     arr = []
-    tArr = tokenize_word(tweet) #NLTK func
-    if index == tArr.size(): return
+    tArr = tweet.split()
+    if index >= len(tArr)-1: return arr
 
     s = tArr[index+1] #range could be oob
-    arr.append(s)
+    if s not in stopwords:
+        arr.append(s)
 
-    for i in range(index+2, tArr.size()): 
+    for i in range(index+2, len(tArr)): 
         s = s + " " + tArr[i]
         arr.append(s)
     return arr
@@ -33,16 +56,35 @@ def addRight(tweet, index):
 
 def addLeft(tweet, index):
     arr = []
-    tArr = tokenize_word(tweet) #NLTK func
-    if index == 0: return
+    tArr = tweet.split() 
+
+    if index == 0: return arr
+    if index == 1:
+        if tArr[0] not in stopwords:
+            arr.append(tArr[0])
+        return arr
 
     s = tArr[index-1]
-    arr.append(s)
+    if s not in stopwords:
+        arr.append(s)
 
-    for i in range(index-2, 0, -1): #check if range will get skipped if oob
+    for i in range(index-2, -1, -1):
         s = tArr[i] + " " + s
         arr.append(s)
     return arr
+
+
+def cleanTweet(tweet, customSW):
+    
+    tweet = tweet.lower()
+    tweet = tweet.replace(".", "")
+
+    tweet = " ".join([word for word in tweet.split(" ") if word not in stopwords and len(word)>1])
+    tweet = " ".join([word for word in tweet.split(" ") if word not in customSW and len(word)>1])
+
+    return tweet
+
+
 # ----------------------------
 
 
@@ -57,7 +99,36 @@ def get_awards(year):
     '''Awards is a list of strings. Do NOT change the name
     of this function or what it returns.'''
     # Your code here
-    awards = ()
+
+    df = pd.read_csv("datasets/dataset2.csv")
+
+    def filterWinners(award):
+        filtered = []
+        for tweet in df["text"]:
+            cTweet = cleanTweet(tweet, [])
+
+            if award in cTweet: 
+                filtered.append([cTweet, award.split()[0]])
+            if ' wins ' in cTweet:
+                filtered.append([cTweet, "wins"])
+            if ' won ' in cTweet:
+                filtered.append([cTweet, "won"])
+            if ' goes to ' in cTweet:
+                filtered.append([cTweet, "goes"])
+            if ' winner ' in cTweet:
+                filtered.append([cTweet, "winner"])
+        return filtered
+
+    awards = {}
+
+    candidates = []
+    for tweet in filterWinners(" "):
+        c = addRight(tweet[0], tweet[0].split().index(tweet[1]))
+        candidates.append(c)
+    awards = sortCandidates(candidates) #should pull top 15-20 though
+
+
+ 
     return awards
 
 def get_nominees(year):
@@ -65,22 +136,46 @@ def get_nominees(year):
     names as keys, and each entry a list of strings. Do NOT change
     the name of this function or what it returns.'''
     # Your code here
+    df = pd.read_csv("datasets/dataset2.csv")
 
+    aw = []
+    for list in OFFICIAL_AWARDS_1315:
+       aw += [word for word in list.split()]
 
     def filterNoms(award):
         filtered = []
-        for tweet in df:
-            if award in tweet and ('nominated' in tweet or 'nominate' in tweet or 'loses' in tweet or 'lost' in tweet):
-                filtered.append(tweet)
+        for tweet in df["text"]:
+
+            cTweet = cleanTweet(tweet, ['best', 'golden', 'globe', 'award', 'globes', 'tv', 'motion', 'picture', 'film', 'picture', 'role', 'performance']) #golden globes needs to be added from config, not hardcoded here !!
+
+            if (bool(set(award.split()) & set(cTweet.split()))): ##figure out better factors
+                cTweet = cleanTweet(cTweet, aw)
+                if ' nominated ' in cTweet:
+                    filtered.append([cTweet, "nominated"])
+                if ' nominate ' in cTweet:
+                    filtered.append([cTweet, "nominate"])
+                if ' nominee ' in cTweet:
+                    filtered.append([cTweet, "nominee"])
+                if ' loses ' in cTweet: #goes to but to is cleaned out
+                    filtered.append([cTweet, "loses"])
+                if ' lost ' in cTweet:
+                    filtered.append([cTweet, "lost"])
+                if ' snub ' in cTweet:
+                    filtered.append([cTweet, 'snub'])
+                if ' snubbed ' in cTweet:
+                    filtered.append([cTweet, "snubbed"])
         return filtered
 
     nominees = {}
     for award in OFFICIAL_AWARDS_1315:
+        print("Award Name: ", award)
         candidates = []
         for tweet in filterNoms(award):
-            c = addRight(tweet)
+            c = addRight(tweet[0], tweet[0].split().index(tweet[1]))
+            c2 = addLeft(tweet[0], tweet[0].split().index(tweet[1]))
             candidates.append(c)
-        nominees[award] = sortCandidates(candidates) #needs to return 5-6? dynamically add # based on number of freq/avg?
+            candidates.append(c2)
+        nominees[award] = sortCandidates(candidates, 5, award) #needs to return 5-6? dynamically add # based on number of freq/avg?
     return nominees
 
 def get_winner(year):
@@ -88,21 +183,42 @@ def get_winner(year):
     names as keys, and each entry containing a single string.
     Do NOT change the name of this function or what it returns.'''
     # Your code here
-<
+    df = pd.read_csv("datasets/dataset2.csv")
+
+    aw = []
+    for list in OFFICIAL_AWARDS_1315:
+       aw += [word for word in list.split()]
+
     def filterWinners(award):
         filtered = []
-        for tweet in df:
-            if award in tweet and ('wins' in tweet or 'won' in tweet or 'goes to' in tweet or 'winner' in tweet):
-                filtered.append(tweet)
+        
+        for tweet in df["text"]:
+            cTweet = cleanTweet(tweet, ['best', 'golden', 'globe', 'award', 'globes', 'tv', 'motion', 'picture', 'film', 'picture', 'role', 'performance']) #golden globes needs to be added from config, not hardcoded here !!
+##should remove words common to all awards
+            if (bool(set(award.split()) & set(cTweet.split()))):
+                
+                cTweet = cleanTweet(cTweet, aw)
+                #print("cleantweet = ", cTweet)
+                if ' wins ' in cTweet:
+                    filtered.append([cTweet, "wins"])
+                if ' won ' in cTweet:
+                    filtered.append([cTweet, "won"])
+                if ' goes ' in cTweet: #goes to but to is cleaned out
+                    filtered.append([cTweet, "goes"])
+                if ' winner ' in cTweet:
+                    filtered.append([cTweet, "winner"])
         return filtered
 
     winners = {}
     for award in OFFICIAL_AWARDS_1315:
+        print("Award Name = ", award)
         candidates = []
         for tweet in filterWinners(award):
-            c = addRight(tweet)
+            c = addRight(tweet[0], tweet[0].split().index(tweet[1]))
+            c2 = addLeft(tweet[0], tweet[0].split().index(tweet[1]))
             candidates.append(c)
-        winners[award] = sortCandidates(candidates)
+            candidates.append(c2)
+        winners[award] = sortCandidates(candidates, 1, award)
     return winners
 
 def get_presenters(year):
@@ -110,9 +226,11 @@ def get_presenters(year):
     names as keys, and each entry a list of strings. Do NOT change the
     name of this function or what it returns.'''
     # Your code here
+    df = pd.read_csv("datasets/dataset2.csv")
+    
     def filterPresenters(award):
         filtered = []
-        for tweet in df:
+        for tweet in df["text"]:
             if award in tweet and ('presenting' in tweet or 'present' in tweet or 'announce' in tweet or 'announcing' in tweet):
                 filtered.append(tweet)
         return filtered
@@ -134,7 +252,7 @@ def pre_ceremony():
     
     preprocess()
     df = pd.read_csv("datasets/dataset2.csv")
-    print(df.head())
+    #print(df["text"])
     
    
 
@@ -146,8 +264,19 @@ def main():
     run when grading. Do NOT change the name of this function or
     what it returns.'''
     # Your Code here
+  
+    
 
     pre_ceremony()
+    n = get_nominees(2013)
+    print("NOMS =", n)
+
+    a1 = ["i", "i love", "i love this", "i love this movie"]
+    a2 = ["this", "this is", "this is my", "this is my favorite"]
+    a3 = ["i", "i am", "i am another", "i am another list"]
+    aAll = [a1, a2, a3]
+
+
     return
 
 if __name__ == '__main__':
